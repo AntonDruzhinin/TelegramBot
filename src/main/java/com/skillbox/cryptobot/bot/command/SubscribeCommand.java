3e8de6797e0c92d17154513ca7637dd86ba585a3
Anton Druzhinin <druzhininantn@gmail.com>
2025-04-14 20:42:50 +0300

    add files

---
+++ src/main/java/com/skillbox/cryptobot/bot/command/SubscribeCommand.java
@@ -0,0 +1,49 @@
+package com.skillbox.cryptobot.bot.command;
+
+import com.skillbox.cryptobot.service.CryptoCurrencyService;
+import com.skillbox.cryptobot.utils.TextUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+
+/**
+ * Обработка команды подписки на курс валюты
+ */
+@Service
+@Slf4j
+@AllArgsConstructor
+public class SubscribeCommand implements IBotCommand {
+
+
+
+
+    @Override
+    public String getCommandIdentifier() {
+        return "subscribe";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Подписывает пользователя на стоимость биткоина";
+    }
+
+    @Override
+    public void processMessage(AbsSender absSender, Message message, String[] arguments) {
+        SendMessage answer = new SendMessage();
+
+
+        answer.setChatId(message.getChatId());
+        try {
+            answer.setText("Здрравствуй, друг " + message.getUserShared().getUserId() +  " USD");
+            absSender.execute(answer);
+        } catch (Exception e) {
+            log.error("Ошибка возникла /get_price методе", e);
+        }
+    }
+}
---
+++ src/main/java/com/skillbox/cryptobot/db/StartDB.java
@@ -0,0 +1,4 @@
+package com.skillbox.cryptobot.db;
+
+public class StartDB {
+}
